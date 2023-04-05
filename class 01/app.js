1227690fb1eed0f9e5b50376c407f5dd046a61df
Maham Babar <mahambabar11@gmail.com>
2023-04-05 09:37:33 -0700

    javascript

---
+++ class 01/app.js
@@ -0,0 +1,42 @@
+alert("Hello World");
+
+let firstName ="maham";
+console.log("firstName " + firstName)
+
+
+// Variable Rules
+// 1. Legal or Illegal names
+// Legal
+// let lastName // CamelCase
+// lastname
+// Allowed Special Characters
+// _ $
+// let $nameWith$Dollar = "";
+// let _nameWith_Dollar = "";
+// Var cannot be start with a number
+// let myName
+
+// Illegal
+// let last name // spaces
+// let my#name
+// reserved words
+
+// alert(firstName);
+
+// Data Types
+
+// 1. String
+let name = 'maham'; 
+console.log ( "string = " +name)
+// examples = "maham", "An Apple", "#", "8" "email@test.com"
+// Any letter, word or sentence is String in JavaScript
+
+// 2. Number
+let myLuckyNumber = 8;
+console.log ("Number = " +myLuckyNumber)
+// example = 4, 37, 0, -34, 4.765
+
+// 3. Boolean
+let isTodayMonday = true;
+console.log( "Boolean = " + isTodayMonday)
+// example = true & false
