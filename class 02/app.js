db39b62e133ba9425997461fc0683796539b72d5
Maham Babar <mahambabar11@gmail.com>
2023-04-05 10:29:35 -0700

    javascript

---
+++ class 02/app.js
@@ -0,0 +1,20 @@
+let name = prompt("what is your name");
+console.log("weolcome to my website " + name)
+
+// BODMAS (Bracket Open Divide Multiply Add+ Substract_)
+
+let num = (5 + 5 * 5) / 5;
+console.log(num);
+
+let modules1 = 5;
+let modules2 = 15;
+console.log (modules1 % modules2);
+
+let randomNumber = Math.random(); 
+randomNumber = randomNumber * 10; 
+randomNumber *= 10; // Short Hand 
+let roundedNumber = Math.round(randomNumber);
+console.log(roundedNumber);
+
+let squreRoot = 8 ** 2;
+console.log(squreRoot);
